41f69ba8ef9588d69251ad4172b8254dd40ff52b
Danny Schmidt <schmid27@stolaf.edu>
2022-04-19 20:02:58 -0500

    database framework file

--- database.cpp
+++ database.cpp
@@ -3,13 +3,13 @@
 #include"database.h"
 using namespace std;
 
+//Constructors
 Database::Database(){
         pLen = 1;
         cLen = 1;
         pStorage = new Post[pLen];
         cStorage = new Community[cLen]];
     }
-
 Database::Database(int p, int c){
     pLen = p;
     cLen = c;
@@ -17,6 +17,7 @@ Database::Database(int p, int c){
     cStorage = new Community[cLen];
 }
 
+//Get storage ids
 int Database::get_post_id(){
     for(int i = 0; i < pLen; i++){
         if(pStorage[i] == 0){
@@ -28,7 +29,6 @@ int Database::get_post_id(){
         return get_post_id();
     }
 }
-
 int Database::get_community_id(){
     for(int i = 0; i < cLen; i++){
         if(cStorage[i] == 0){
@@ -41,6 +41,7 @@ int Database::get_community_id(){
     }
 }
 
+//Expand storage
 void Database::expand_pStorage(){
     pLen = pLen * 2;
     Post* tmp = new Post[pLen];
@@ -50,7 +51,6 @@ void Database::expand_pStorage(){
     delete [] pStorage;
     pStorage = tmp;
 }
-
 void Database::expand_cStorage(){
     cLen = cLen * 2;
     Community* tmp = new Community[pLen];
@@ -60,20 +60,21 @@ void Database::expand_cStorage(){
     delete [] cStorage;
     cStorage = tmp;
 }
+
+//add item to storage
 void Database::add_post(Post* p){
     int i = get_post_id();
     pStorage[i] = p;
 }
-
 void Database::add_community(Community* c){
     int i = get_community_id();
     cStorage[i] = c;
 }
 
+//get item ids
 Post* Database::get_post(int id){
     return pStorage[id];
 }
-
 Community* Database::get_community(int id){
     return cStorage[id];
 }
