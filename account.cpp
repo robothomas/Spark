7ed81bb4819e94b063676e6fb1cc44c20e55e750
Danny Schmidt <schmid27@stolaf.edu>
2022-04-19 19:56:26 -0500

    account files

--- account.cpp
+++ account.cpp
@@ -3,67 +3,125 @@
 #include"account.h"
 using namespace std;
 
-string get_user(){return user;}
+//Constructor
+Account::Account(){
+    ypl = 1;
+    spl = 1;
+    scl = 1;
+    user = " ";
+    password = " ";
+    email = " ";
+}
+Account::Account(string u, string p, string e){
+    ypl = 1;
+    spl = 1;
+    scl = 1;
+    user = u;
+    password = p;
+    email = e;
+}
 
-string get_password(){return password;}
-
-string get_email(){return email;}
-
-void change_password(string pw){
+//Login info
+string Account::get_user(){return user;}
+string Account::get_password(){return password;}
+string Account::get_email(){return email;}
+void Account::change_password(string pw){
     password.clear();
     password += pw;
 }
-
-void change_email(string em){
+void Account::change_email(string em){
     email.clear();
     email += em;
 }
 
-void add_saved_post(Post* p){
-
+//Saved posts
+void Account::add_saved_post(Post* p){
+    int i = get_saved_posts_id();
+    saved_posts[i] = p;
+}
+void Account::remove_saved_post(int id){
+    saved_posts[id] = 0; 
 }
 
-void remove_saved_post(Post* p){
-
+//Saved communities
+void Account::add_saved_community(Community* c){
+    int i = get_saved_communities_id();
+    saved_communities[i] = c;
+}
+void Account::remove_saved_community(int id){
+    saved_communities[id] = 0;
 }
 
-void add_saved_community(Community* c){
-
+//New posts
+void Account::add_new_post(Post* p){
+    int i = get_your_posts_id();
+    your_posts[i] = p;
+}
+void Account::remove_your_post(int id){
+    your_posts[id] = 0;
 }
 
-void removed_saved_community(Community* c){
-
+//get ids
+int Account::get_your_posts_id(){
+    for(int i = 0; i < ypl; i++){
+        if(your_posts[i] == 0){
+            return i;
+        }
+    }
+    if(your_posts[ypl] != 0){
+        expand_your_posts();
+        return get_your_posts_id();
+    }
+}
+int Account::get_saved_post_id(){
+    for(int i = 0; i < spl; i++){
+        if(saved_posts[i] == 0){
+            return i;
+        }
+    }
+    if(saved_posts[spl] != 0){
+        expand_saved_posts();
+        return get_saved_posts_id();
+    }
+}
+int Account::get_saved_communities_id(){
+    for(int i = 0; i < scl; i++){
+        if(saved_communities[i] == 0){
+            return i;
+        }
+    }
+    if(saved_communities[scl] != 0){
+        expand_saved_communities();
+        return get_saved_communities_id();
+    }
 }
 
-void add_new_post(Post* p){
-
+//Exapnd libraries
+void Account::expand_your_posts(){
+    ypl = ypl * 2;
+    Post* tmp = new Post[ypl];
+    for(int i = 0; i < ypl; i++){
+        tmp[i] = your_posts[i];
+    }
+    delete [] your_posts;
+    your_posts = tmp;
 }
-int get_your_posts_id(){
-
+void Account::expand_saved_posts(){
+    spl = spl * 2;
+    Post* tmp = new Post[spl];
+    for(int i = 0; i < spl; i++){
+        tmp[i] = saved_posts[i];
+    }
+    delete [] saved_posts;
+    saved_posts = tmp;
 }
-
-int get_saved_post_id(){
-
+void Account::expand_saved_communities(){
+    scl = scl * 2;
+    Community* tmp = new Community[scl];
+    for(int i = 0; i < scl; i++){
+        tmp[i] = saved_communities[i];
+    }
+    delete [] saved_communities;
+    saved_communities = tmp;
 }
-
-int get_saved_communities_id(){
-
-}
-
-void expand_your_posts(){
-
-}
-
-void expand_saved_posts(){
-
-}
-
-void expand_saved_communities(){
-
-}
-
-
-
-
-
 
--- account.h
+++ account.h
@@ -2,6 +2,9 @@
 #define __COMMUNITY_H__
 
 class Account{
+    int ypl;
+    int spl;
+    int scl;
     string user;
     string password;
     string email;
@@ -9,16 +12,19 @@ class Account{
     Post* saved_posts;
     Community* saved_communities;
 public:
+    account();
+    account(string user, string password, string email);
     string get_user();
     string get_password();
     string get_email();
     void change_password(string pw);
     void change_email(string em);
     void add_saved_post(Post* p);
-    void remove_saved_post(Post* p);
+    void remove_saved_post(int id);
     void add_saved_community(Community* c);
-    void removed_saved_community(Community* c);
+    void remove_saved_community(int id);
     void add_new_post(Post* p);
+    void remove_your_post(int id);
     int get_your_posts_id();
     int get_saved_post_id();
     int get_saved_communities_id();
