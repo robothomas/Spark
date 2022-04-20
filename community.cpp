cb9baedf12529fa6f0f44d28a16a2129ad927b98
Danny Schmidt <schmid27@stolaf.edu>
2022-04-19 20:02:04 -0500

    community framework file

--- community.cpp
+++ community.cpp
@@ -3,6 +3,7 @@
 #include"community.h"
 using namespace std;
 
+//Constructors
 Community::Community();{
     id = Database.get_community_id();
     len = 1;
@@ -10,7 +11,6 @@ Community::Community();{
     description = Text();
     posts = new Post[len];
 }
-
 Community::Community(Text *t, Text *d){
     id = Database.get_community_id();
     len = 1;
@@ -19,37 +19,37 @@ Community::Community(Text *t, Text *d){
     posts = new Post[len];
 }
 
+//Title
 Text Community::get_title(){ 
     return title;
 }
-
-Text Community::get_description(){
-    return description;
-}
-
 void Community::change_title(Text *t){
     delete [] title;
     title = t;
 }
 
+//Description
+Text Community::get_description(){
+    return description;
+}
 void Community::change_description(Text *d){
     delete [] description;
     description = d;
 }
 
+//Alter posts
 void Community::add_post(Post *p){
     int i = get_open_id();
     posts[i] = p; 
 }
-
 void Community::remove_post(int id){
     posts[id] = 0;
 }
-
 Post* Community::get_post(int id){
     return posts[id];
 }
 
+//post array
 int Community::get_open_id(){
     for(int i = 0; i < len; i++){
         if(posts[i] == 0){
@@ -61,7 +61,6 @@ int Community::get_open_id(){
         return get_open_id();
     }
 }
-
 void Community::expand_posts(){
     len = len * 2;
     Post* tmp = new Post[len];
