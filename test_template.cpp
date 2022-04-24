e24dcf7f3c6f5e533a2575c232ee343ffabb5ea7
Dylan Thomas <thomas31@stolaf.edu>
2022-04-24 18:21:58 -0500

    split several parts of new post into yaml templates

--- test_template.cpp
+++ test_template.cpp
@@ -12,9 +12,10 @@ void addRatingBulb(bool on, float left) {
 
 void addRatingArray(int rating) {
     for (int i = 0; i < 5; i++) {
-        float left = 0.01;
+        float left = 1;
         if (i < rating) {
             addRatingBulb(true, left);
+            left += 19.5;
         } else {
             addRatingBulb(false, left);
         }
@@ -23,6 +24,10 @@ void addRatingArray(int rating) {
 
 int main() {
   add_yaml("searchBar.yaml", {{"top", 5}});
+  add_yaml("postCommunity.yaml", {"width", 33}, {"left", 10})
+  add_yaml("postCommunity.yaml", {"width", 36}, {"left", 45})
+  add_yaml("textBox.yaml", {{"top", 25}, {"height", 8}, {"fontSize", 18}}, {"text_index", 0})
+  add_yaml("textBox.yaml", {{"top", 35}, {"height", 33}, {"fontSize", 15}, {"text_index", 1}})
   addRatingArray(3);
   add_yaml("baseTemplate.yaml");
   _write_global_yaml_to_file("react.yaml");
