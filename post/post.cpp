096e78466b03bc11ef779c0933f71f15173ca549
Dylan Thomas <thomas31@stolaf.edu>
2022-04-26 21:52:54 -0500

    post troubleshooting

--- post/post.cpp
+++ post/post.cpp
@@ -11,19 +11,30 @@ Post::Post() {
     communityIDs[0] = 0;
 }
 
-Post::Post(int id_num, int communityID, string postTitle) : id(id_num), title(postTitle) {
+Post::Post(int id_num, int communityID, string postTitle) {
+    id = id_num;
+    title = postTitle;
     communityIDs = new int[1];
     communityIDs[0] = communityID;
 }
 
-Post::Post(int id_num, int communityID, string postTitle; Text postDescription) : id(id_num), title(postTitle) {
+Post::Post(int id_num, int communityID, string postTitle, Text postDescription) {
+    id = id_num;
+    title = postTitle;
     communityIDs = new int[1];
     communityIDs[0] = communityID;
     description = postDescription;
 }
 
-Post::Post(int id_num, int communityIDs[], string postTitle);
-Post::Post(int id_num, int communityIDs[], string postTitle, Text postDescription);
+/*
+Post::Post(int id_num, int communityIDs[], string postTitle) {
+
+}
+
+Post::Post(int id_num, int communityIDs[], string postTitle, Text postDescription) {
+
+}
+*/
 
 int Post::get_id() {
     return id;
@@ -38,13 +49,13 @@ Text Post::get_description() {
 }
 
 void Post::read_from(char* mem){
-    id = _get_int(mem);
+    id = _get_int(mem, 2);
     mem += 2;
     int j = 0;
-    while( _get_char(mem) != "~"){j++;}
-    communityID = new int[j];
+    while( _get_char(mem, 1) != "~"){j++;}
+    communityIDs = new int[j];
     for(int i = 0; i < j; i++){
-        communityID[i] = _get_int(mem);
+        communityIDs[i] = _get_int(mem, 2);
         mem += 2;
     }
     title = _get_tilde_terminated_string(mem); 
@@ -62,8 +73,8 @@ void Post::write_to(char *mem) {
     _put_char('\n', mem, 1);
     mem++;
 
-    for (i = 0; communityIDs[i]; i++) {
-        put_int(communityIDs[i], mem, 2);
+    for (int i = 0; communityIDs[i]; i++) {
+        _put_int(communityIDs[i], mem, 2);
         mem += 2;
     }
 
