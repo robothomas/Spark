37641d9fd4680ade149b7261ad3fb80d1148a70e
Dylan Thomas <thomas31@stolaf.edu>
2022-05-01 16:06:05 -0500

    ideagenerator outline

--- idea_generator/IdeaGenerator.h
+++ idea_generator/IdeaGenerator.h
@@ -1,10 +1,40 @@
 #ifndef __IDEAGENERATOR_H__
 #define __IDEAGENERATOR_H__
 
+#include <string>
+using namespace std;
+
 class IdeaGenerator {
-    void read_from();
-    void write_to();
-    void display();
+    private:
+        int format;
+        int type;
+        int recent;
+        int difficulty;
+
+    public:
+        string searchQuery;
+        IdeaGenerator();
+        IdeaGenerator(const IdeaGenerator &idea);
+        IdeaGenerator operator=(const IdeaGenerator &idea);
+
+        string getFormat();
+        string getType();
+        string getRecency();
+        int getDifficulty();
+
+        void setFormat(string format);
+        void setFormat(int format);
+        
+        void setDifficulty(int difficulty);
+
+        void setType(string type);
+        void setType(int type);
+
+        void setRecency(string recency);
+        void setRecency(int recency);
+
+        void read_from();
+        void write_to();
 }
 
 #endif
