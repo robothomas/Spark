b8d59314407ac3c843cf9fd7e772121bed242809
Dylan Thomas <thomas31@stolaf.edu>
2022-05-15 22:40:09 -0500

    commented out cerr statements (makes display look off)

--- state/State.cpp
+++ state/State.cpp
@@ -21,29 +21,29 @@ void State::read_from(char *mem) {
     idea_offset = 2;
 
     ideaGen.read_from(mem);
-    cerr << "query: " << ideaGen.searchQuery << endl;
-    cerr << "recency: " << ideaGen.getRecency() << endl;
-    cerr << "difficulty: " << ideaGen.getDifficulty() << endl;
+    //cerr << "query: " << ideaGen.searchQuery << endl;
+    //cerr << "recency: " << ideaGen.getRecency() << endl;
+    //cerr << "difficulty: " << ideaGen.getDifficulty() << endl;
 
     mem += ideaGen.size_in_bytes();
     account_offset = idea_offset + ideaGen.size_in_bytes();
 
     account.read_from(mem);
-    cerr << "user: " << account.get_user() << endl;
-    cerr << "password: " << account.get_password() << endl;
-    cerr << "email: " << account.get_email() << endl;
+    //cerr << "user: " << account.get_user() << endl;
+    //cerr << "password: " << account.get_password() << endl;
+    //cerr << "email: " << account.get_email() << endl;
     
     mem += account.size_in_bytes();
     newPost_offset = account_offset + account.size_in_bytes();
 
     //newPost.read_from(mem);
 
-    cerr << "ID: " << newPost.get_id() << endl;
-    cerr << "Community IDs: " << newPost.getCommunityIDs() << endl;
-    cerr << "Title: " << newPost.get_title() << endl;
-    cerr << "Description: " << newPost.get_description().getText() << endl;
-    cerr << "Community query: " << newPost.getQuery() << endl;
-    cerr << "Communities: " << newPost.getCommunities() << endl;
+    //cerr << "ID: " << newPost.get_id() << endl;
+    //cerr << "Community IDs: " << newPost.getCommunityIDs() << endl;
+    //cerr << "Title: " << newPost.get_title() << endl;
+    //cerr << "Description: " << newPost.get_description().getText() << endl;
+    //cerr << "Community query: " << newPost.getQuery() << endl;
+    //cerr << "Communities: " << newPost.getCommunities() << endl;
 
     //mem += account.size_in_bytes(); // not implemented yet
 
